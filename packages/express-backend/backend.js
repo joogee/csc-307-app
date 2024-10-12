88a91dc8de61d4e861d1c175bd12138a2a6f80bf
Joanna Tondreau <jntondreau@gmail.com>
2024-10-11 23:48:46 -0700

    Add Users routes

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,6 +1,35 @@
 // backend.js
 import express from "express";
 
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
 const app = express();
 const port = 8000;
 
@@ -8,6 +37,66 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+const findUserByName = (users, name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+const findUserByJob = (users, job) => {
+  return users["users_list"].filter((user) => user["job"] === job);
+};
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  const job = req.query.job;
+  var result = users;
+  if (name != undefined) {
+    result = findUserByName(result, name);
+    result = { users_list: result };
+  }
+  if (job != undefined) {
+    result = findUserByJob(result, job);
+    result = { users_list: result };
+  }
+  res.send(result);
+});
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    users["users_list"] = users["users_list"].filter(
+      (user) => user["id"] !== id
+    );
+  }
+  res.send();
 });
 
 app.listen(port, () => {
